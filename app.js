3067ae95fc2a2157251410201c0f2f8ab9f8d968
807065214@qq.com <dongwanlong@51talk.com>
2018-03-05 14:44:16 +0800

    dwl

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var cors = require("cors");
+var path = require("path");
+var fs = require("fs");
 var bodyParser = require('body-parser');
 var router = require("./router/router.js");
 var app = express();
@@ -8,6 +10,17 @@ app.use(cors({}));
 app.use(bodyParser.json());
 app.use("/", router);
 app.use('/static', express.static('public'));
+
+//目录初始化
+var publicDir = path.resolve(__dirname, './public');
+var imgsDir = path.resolve(__dirname, './public/imgs');
+var sourcesDir = path.resolve(__dirname, './public/sources');
+var uploadDir = path.resolve(__dirname, './public/upload');
+if(!fs.existsSync(publicDir))fs.mkdirSync(publicDir);
+if(!fs.existsSync(imgsDir))fs.mkdirSync(imgsDir);
+if(!fs.existsSync(sourcesDir))fs.mkdirSync(sourcesDir);
+if(!fs.existsSync(uploadDir))fs.mkdirSync(uploadDir);
+
 
 var server = app.listen(80, function () {
     var host = server.address().address;
